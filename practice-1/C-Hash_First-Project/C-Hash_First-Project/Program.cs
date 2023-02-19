96233c914662d1f747153795b8c9c61d3c7c565a
Junaid Khalid <57348907+Junaid20001@users.noreply.github.com>
2023-02-19 13:25:20 +0500

    Add files via upload

---
+++ practice-1/C-Hash_First-Project/C-Hash_First-Project/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Hash_First_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int x, y, z;
+              Console.Write("Enter number 1 : ");    
+              x = Convert.ToInt32(Console.ReadLine());    //convert string to int//
+              Console.Write("Enter number 2 : ");
+              y = Convert.ToInt32(Console.ReadLine());
+              z = x + y;
+              Console.WriteLine("The sum of " + x + " & " + y + " is " + z);
+              Console.ReadLine(); */
+
+            //int number1; //declaration
+            //number1 = 5; //Intialization
+            /* int number = 60;
+             float numberFlt = 6.8F;
+             string s = "Hello";
+             double d = 5.321;
+             UInt16 number1 = 88;
+             Console.WriteLine(number + " " + numberFlt + " " + s + " " + d + " " + number1);
+             Console.ReadLine(); */
+
+
+          /*  int number;
+            Console.WriteLine("Enter integer : ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            float numberFlt;
+            Console.WriteLine("Enter integerFlt : ");
+            numberFlt = Convert.ToSingle(Console.ReadLine());
+
+            string s;
+            Console.WriteLine("Enter String : ");
+            s = Console.ReadLine();
+
+            double d;
+            Console.WriteLine("Enter double float : ");
+            d = Convert.ToDouble(Console.ReadLine());
+
+            UInt32 numUnsigned;
+            Console.WriteLine("Enter Unsigned integer : ");
+            numUnsigned = Convert.ToUInt32(Console.ReadLine());
+
+            Console.WriteLine(number + " " + numberFlt + " " + s + " " + d + " " + numUnsigned);
+            Console.ReadLine();  */
+          
+
+
+
+
+
+        }
+    }
+}
